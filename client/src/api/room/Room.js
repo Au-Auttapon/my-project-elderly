ddec478c46cedaa55ac104964a330df2d14bf527
Au-Auttapon <auttapon.apr@gmail.com>
2024-11-04 22:26:58 -0800

    fix api room

--- client/src/api/room/Room.js
+++ client/src/api/room/Room.js
@@ -7,13 +7,12 @@ const Room = {
     get: async () => {
         try {
             const res = await Axios.get(`${url}/rooms`); // Use await to get the response
-            console.log("res", res.data); // Log the data
             return res.data; // Return the data
         } catch (error) {
             console.error(error); // Log the error
             throw error; // Rethrow the error if you want to handle it later
         }
-    }
+    },
 };
 
 export default Room; // Export the Room object
